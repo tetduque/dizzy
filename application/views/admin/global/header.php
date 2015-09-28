55e90e50fa5e08f79cf822477cb5c99dad438720
Tet Duque <matet16[gmail.com>
2015-09-28 11:05:52 +0900

    added new data

---
+++ application/views/admin/global/header.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<title>DizzyPoint 管理画面</title>
+
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+
+</head>
+
+<body>
+
+<div id="menu">
+	<ul class="nav nav-pills nav-stacked">
+	  <li role="category" class="active"><a href="#">カテゴリーの一覧 (Categories)</a>
+		<br>
+		>> カテゴリーから種類や生体の検索、編集
+	  </li>
+	  <li role="kind"><a href="#">種類の一覧 (Kind/Items)</a>
+		<br>
+		>> 種類から生体、生体の写真を検索、編集
+	  </li>
+	  <li role="individual"><a href="#">生体の一覧 (Invidual/Details)</a>
+		<br>
+		>> 生体、生体の写真の検索、編集
+	  </li>
+	  <li role="price"><a href="#">特別価格の設定(Price)</a>
+		<br>
+		>> SaleやASKなど生体特別価格の設定
+	  </li>
+	  <li role="sizecode"><a href="#">サイズコードの一覧(Size Code)</a>
+		<br>
+		>> サイズコードの一覧から編集
+	  </li>
+	  <li role="image"><a href="#">写真の一覧 (images)</a>
+		<br>
+		>> 写真の変更,写真の登録はここから出来ません、生体一覧からお願いします
+	  </li>
+	  <li role="orderhistory"><a href="#">注文履歴の一覧 (Order History)</a>
+		<br>
+		>> 注文履歴の確認
+	  </li>
+	  <li role="what"><a href="#">お知らせの一覧 (What)</a>
+		<br>
+		>> お知らせの一覧の編集
+	  </li>
+	  <li role="rssupdate"><a href="#">RSS更新 (RSS)</a>
+		<br>
+		>> RSS更新
+	  </li>
+	  <li role="label"><a href="#">ラベル作成 (Label)</a>
+		<br>
+		>> ラベル作成
+	  </li>
+	  <li role="blog"><a href="#">Blog更新 (Blog)</a>
+		<br>
+		>> Blog更新 ユーザ名:dizzypoint　パス:dzypt0915
+	  </li>
+</div>
+
+
